fecd91f045cf1313ae4f1a31bed596eeadecba3e
Craig Peterson <192540+captncraig@users.noreply.github.com>
2020-12-02 19:43:07 -0500

    remove import from template

--- tpl.sh
+++ tpl.sh
@@ -2,13 +2,13 @@
 
 DAY=$1
 FILE=day$DAY.go     
-if [ -f $FILE ]; then
-   echo "File $FILE exists."
-   exit 1
-fi
 
 # get input and save to clipboard (osx only)
 # must set $ADVENT_COOKIE first
 curl --cookie "session=$ADVENT_COOKIE" https://adventofcode.com/2020/day/$DAY/input | pbcopy
 
+if [ -f $FILE ]; then
+   echo "File $FILE exists."
+   exit 1
+fi
 sed "s/NN/${DAY}/g" template > $FILE
