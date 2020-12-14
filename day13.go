11c702a75cdbd60046f1370446e4f6abc9f46a20
Craig Peterson <192540+captncraig@users.noreply.github.com>
2020-12-14 16:29:45 -0500

    I did p2 in sheets

--- day13.go
+++ day13.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"log"
 	"strings"
 )
 
 func day13(input string) (p1Result, p2Result int) {
-	input = `
-7,13,x,x,59,x,31,19`
+	//input = `939
+	//7,13,x,x,59,x,31,19`
 	lines := Lines(input)
 	current := Atoi(lines[0])
-	log.Println(current)
 	split := strings.Split(lines[1], ",")
 	busses := []int{}
 	for _, bus := range split {
 		if bus != "x" {
 			busses = append(busses, Atoi(bus))
+		} else {
+			busses = append(busses, -1)
 		}
 	}
 	// p1
 	min := 9999
 	for _, bus := range busses {
+		if bus == -1 {
+			continue
+		}
 		goneBy := current % bus
 		remaining := bus - goneBy
 		if remaining < min {
@@ -29,6 +32,48 @@ func day13(input string) (p1Result, p2Result int) {
 		}
 	}
 
+	// p2
+	// modList := map[int64]int64{}
+
+	// max := 0
+	// for i, n := range busses {
+	// 	if n == -1 {
+	// 		continue
+	// 	}
+	// 	offset := (n - i)
+	// 	for offset < 0 {
+	// 		offset += n
+	// 	}
+	// 	offset %= n
+	// 	modList[int64(n)] = int64(offset)
+	// 	if n > max {
+	// 		max = n
+	// 	}
+	// }
+
+	// log.Println(modList, max)
+	// incr := int64(max)
+	// start := int64(0)
+	// for start%incr != modList[incr] {
+	// 	start++
+	// }
+	// delete(modList, max)
+	// for {
+	// 	log.Println(start)
+	// 	ok := true
+	// 	for n, mod := range modList {
+	// 		if start%n != mod {
+	// 			ok = false
+	// 			break
+	// 		}
+	// 	}
+	// 	if ok {
+	// 		log.Println(start)
+	// 		p2Result = int(start)
+	// 		return
+	// 	}
+	// 	start += incr
+	// }
 	return
 }
 
